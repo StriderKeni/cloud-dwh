e6f1d3f5029bad0c0072df8a9cc979c7b68073fc
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-04-17 14:43:12 -0400

    Added print statements for insert data into staging and analytics tables

--- etl.py
+++ etl.py
@@ -1,18 +1,23 @@
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries
+from sql_queries import copy_table_queries, insert_table_queries, table_list
 
 
-# def load_staging_tables(cur, conn):
-#     for query in copy_table_queries:
-#         cur.execute(query)
-#         conn.commit()
+def load_staging_tables(cur, conn):
+    for query, table_name in zip(copy_table_queries, table_list):
+        print("\nLoading data to staging table {}".format(table_name))
+        print("This process may take a moment. Please wait...")
+        cur.execute(query)
+        conn.commit()
+        print("Data loaded succesfully to {}".format(table_name))
 
 
 def insert_tables(cur, conn):
-    for query in insert_table_queries:
+    for query, table_name in zip(insert_table_queries, table_list[2::]):
+        print("\nInserting data into table {}".format(table_name))
         cur.execute(query)
         conn.commit()
+        print("Data inserted succesfully into {}".format(table_name))
 
 
 def main():
@@ -23,7 +28,7 @@ def main():
         "host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
-    # load_staging_tables(cur, conn)
+    load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
     conn.close()
